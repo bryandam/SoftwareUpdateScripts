13e1fea5ec68f7d86f6dda6629347d4b94518baa
Bryan Dam <bryandam@users.noreply.github.com>
2018-06-25 13:55:41 -0400

    Version 2.3
    Fixed merge screw up that reverted to an earlier version that did not have the stand-alone WSUS feature.
    Using WhatIf will force the script to run regardless of the 24-hour timeout.
    Make DeclineByTitle explicitly case-insensitive.

---
+++ Invoke-DGASoftwareUpdateMaintenance/Plugins/Disabled/Decline-Office365Editions.ps1
@@ -0,0 +1,76 @@
+﻿<#
+.SYNOPSIS
+Decline updates for editions of Windows 10 your organization does not support.
+.DESCRIPTION
+Decline updates for editions of Windows 10 your organization does not support.  If the LatestVersionOnly is left to its default value of True then for each edition only the latest version will be retained and all others declined.
+.NOTES
+You must un-comment the $SupportedEditions variable and add the editions your organization supports.
+The KnownEditions variable holds a list of known editions to _try_ and prevent the script from going rogue if MS decides to change the naming scheme.  If ... or when ... they do this will need to be updated.
+Written By: Bryan Dam
+Version 1.0: 10/28/17
+#>
+#Un-comment and add elements to this array for editions you support.
+#Note: You must escape any parenthesis with the forward slash.  Ex.: "Office 365 Client Update - Monthly Channel \(Targeted\) Version"
+#$SupportedEditions = @("Office 365 Client Update - Monthly Channel \(Targeted\) Version","Office 365 Client Update - Semi-annual Channel Version")
+
+#Set this to $True to decline all but the latest version of each editions or $False to ignore versions.
+$LatestVersionOnly=$True
+
+#If Microsoft decides to change their naming scheme you will need to udpate this variable to support the new scheme.
+$KnownEditions=@("Office 365 Client Update - First Release for Deferred Channel","Office 365 Client Update - First Release for Current Channel","Office 365 Client Update - Current Channel","Office 365 Client Update - Deferred Channel", "Office 365 Client Update - Monthly Channel Version","Office 365 Client Update - Monthly Channel \(Targeted\) Version","Office 365 Client Update - Semi-annual Channel Version","Office 365 Client Update - Semi-annual Channel \(Targeted\) Version")
+Function Invoke-SelectUpdatesPlugin{
+
+
+    $DeclinedUpdates = @{}
+    If (!$SupportedEditions){Return $DeclinedUpdates}    
+    $maxVersions = @{}
+    $Office365Updates = ($Updates | Where{$_.ProductTitles -eq "Office 365 Client" -and !$_.IsDeclined })    
+    
+    #Loop through the updates and editions and determine the highest version number per edition.
+    If ($LatestVersionOnly){
+        ForEach ($Update in $Office365Updates){
+            ForEach ($KnownEdition in $KnownEditions){
+                If($Update.Title -match $KnownEdition){
+                    If ($Update.Title -match "Version (\d+)"){
+                        If ($Matches[1] -gt $maxVersions[$KnownEdition]) {$maxVersions.Set_Item($KnownEdition,$Matches[1])}
+                    }
+                }
+            }
+        }
+    }
+
+    #Loop through the updates and decline the desired updates.
+    ForEach ($Update in $Office365Updates){
+         ForEach ($KnownEdition in $KnownEditions){
+
+                #Verify that the update is a known edition.
+                If($Update.Title -match $KnownEdition){
+                                                        
+                    #Determine if the update is a supported version and what known edition it is.
+                    $FoundSupportedVersion = $False
+                    $FoundEdition=""
+                    ForEach($SupportedEdition in $SupportedEditions){
+                        If($Update.Title -match $SupportedEdition){
+                            $FoundSupportedVersion = $True
+                            $FoundEdition = $KnownEdition
+                        }
+                    }
+
+                    #If a supported version was found and we're only keeping the latest version.
+                    If ($FoundSupportedVersion -and $LatestVersionOnly){
+                        #Decline updates that are not the latest version.
+                        If ($Update.Title -notlike "*Version $($maxVersions[$KnownEdition])*"){
+                            $DeclinedUpdates.Set_Item($Update.Id.UpdateId,"Office 365 Updates: Version")
+                        }
+                    #If a supported version was not found then decline it.
+                    } Else {
+                        $DeclinedUpdates.Set_Item($Update.Id.UpdateId,"Office 365 Updates: Edition")
+                    }                                        
+                } #If a Known Edition                 
+            } #ForEach Edition
+
+
+        
+    } #Office 365 Updates
+    Return $DeclinedUpdates
+}
---
+++ Invoke-DGASoftwareUpdateMaintenance/Plugins/Disabled/Decline-Windows10Versions.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.SYNOPSIS
+Decline updates for defined versions of Windows 10.
+.DESCRIPTION
+Decline updates for defined versions of Windows 10.
+.NOTES
+You must un-comment the $UnsupportedVersions variable and add the versions your organization does not support.
+Written By: Bryan Dam
+Version 1.0: 10/25/17
+#>
+
+#Un-comment and add elements to this array for versions you no longer support.
+#$UnsupportedVersions = @("1511")
+Function Invoke-SelectUpdatesPlugin{
+
+    $DeclinedUpdates = @{}
+    If (!$UnsupportedVersions){Return $DeclinedUpdates}
+
+    $Windows10Updates = ($Updates | Where{$_.ProductTitles -eq "Windows 10" -and !$_.IsDeclined })
+    
+    #Loop through the updates and decline any that match the version.
+    ForEach ($Update in $Windows10Updates){
+
+        #If the title contains a version number.
+        If ($Update.Title -match "Version \d\d\d\d"){
+            
+            #Capture the version number.
+            $Version = $matches[0].Substring($matches[0].Length - 4)
+            
+            #If the version number is in the list then decline it.
+            If ($UnsupportedVersions.Contains($Version)){
+                $DeclinedUpdates.Set_Item($Update.Id.UpdateId,"Windows 10 Version: $($Version)")
+            }
+        }
+    }
+    Return $DeclinedUpdates
+}
